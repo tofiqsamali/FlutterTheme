59b8002ef931de4a7d90dcd929f318b7949fdf10
Tofiq Samali <t.samali5987@gmail.com>
2019-12-17 07:11:27 +0330

    modified:   lib/main.dart 	new file:   lib/theme_notifier.dart 	new file:   lib/themes.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/theme_notifier.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_theme/themes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeNotifier with ChangeNotifier {
+  ThemeData _themeData;
+
+  ThemeNotifier(this._themeData);
+
+  getTheme() => _themeData;
+
+  setTheme(ThemeData themeData) async {
+    _themeData = themeData;
+    notifyListeners();
+  }
+}
+
+void onThemeChanged(bool value, ThemeNotifier themeNotifier) async {
+  (value)
+      ? themeNotifier.setTheme(darkTheme)
+      : themeNotifier.setTheme(lightTheme);
+  var prefs = await SharedPreferences.getInstance();
+  prefs.setBool('darkMode', value);
+}
